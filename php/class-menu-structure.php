56fe9e8b7981defc249f83fb1dcc0cd5d8de908a
Diede Exterkate <diede@yoast.com>
2016-07-19 16:49:25 +0200

    removed incompatible activeOn value. This should be set in the metabox in the backend

--- php/class-menu-structure.php
+++ php/class-menu-structure.php
@@ -320,7 +320,6 @@ class Menu_Structure {
 				'type'     => self::COURSES_TYPE,
 				'activeOn' => array(
 					$this->academyBaseUrl  => array(),
-					$this->yoastComBaseUrl . 'academy/' => array(),
 					$this->yoastComBaseUrl => array( 'yoast_courses' ),
 				),
 			)
